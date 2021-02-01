7686b489b197699fad0f60a6e2854f69ff601f23
Aleksey Lyubushkin <5841459@gmail.com>
2021-02-01 13:36:44 +0300

    Add change after feedback.

--- FillColors/ViewController.swift
+++ FillColors/ViewController.swift
@@ -21,62 +21,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        redSlider.minimumTrackTintColor = .red
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
-        
-        greenSlider.minimumTrackTintColor = .green
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 1
-        
-        blueSlider.minimumTrackTintColor = .blue
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 1
-        
-
-        redLabel.text = "\(redSlider.value)"
-        greenLabel.text = "\(greenSlider.value)"
-        blueLabel.text = "\(blueSlider.value)"
-        
-        redLabel.textColor = .white
-        greenLabel.textColor = .white
-        blueLabel.textColor = .white
-        
         fillingColorView.layer.cornerRadius = 20
-        fillingColorView.backgroundColor =
-            .init(red: CGFloat(redSlider.value),
-                  green: CGFloat(greenSlider.value),
-                  blue: CGFloat(blueSlider.value),
-                  alpha: 1)
-
+        fillingColorView.backgroundColor = .gray
     }
-
-    @IBAction func redSliderAction() {
+    
+    @IBAction func sliderAction() {
+        
         redLabel.text = "\(round(100 * redSlider.value) / 100)"
-        fillingColorView.backgroundColor =
-            .init(red: CGFloat(redSlider.value),
-                  green: CGFloat(greenSlider.value),
-                  blue: CGFloat(blueSlider.value),
-                  alpha: 1)
-    }
-    
-    @IBAction func greenSliderAction() {
         greenLabel.text = "\(round(100 * greenSlider.value) / 100)"
-        fillingColorView.backgroundColor =
-            .init(red: CGFloat(redSlider.value),
-                  green: CGFloat(greenSlider.value),
-                  blue: CGFloat(blueSlider.value),
-                  alpha: 1)
-    }
-    
-    @IBAction func blueSliderAction() {
         blueLabel.text = "\(round(100 * blueSlider.value) / 100)"
-        fillingColorView.backgroundColor =
-            .init(red: CGFloat(redSlider.value),
-                  green: CGFloat(greenSlider.value),
-                  blue: CGFloat(blueSlider.value),
-                  alpha: 1)
+        
+        fillingColorView.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
     
 }
